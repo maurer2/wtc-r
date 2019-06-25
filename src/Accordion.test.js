4cba7334fa2617ff34c9930577cd7a80628f8359
Martin <mr.mmaurer+work@gmail.com>
2019-06-25 23:34:44 +0200

    Added a few tests

--- src/Accordion.test.js
+++ src/Accordion.test.js
@@ -9,6 +9,11 @@ describe('Accordion', () => {
       id: '1',
       question: 'Question 1',
     },
+    {
+      answer: 'Answer 2',
+      id: '2',
+      question: 'Question 2',
+    },
   ];
   let wrapper;
 
@@ -18,9 +23,43 @@ describe('Accordion', () => {
 
   test('should render correctly', () => {
     expect(wrapper).toMatchSnapshot();
+
+    const firstTitle = wrapper.find('.title-link').first();
+
+    firstTitle.simulate('click', { preventDefault: () => {} });
+
+    expect(wrapper).toMatchSnapshot();
   });
 
   test('has wrapper class', () => {
     expect(wrapper.find('.accordion').exists()).toBe(true);
+  });
+
+  test('has question and answers', () => {
+    expect(wrapper.find('.title').exists()).toBe(true);
+    expect(wrapper.find('.answer').exists()).toBe(true);
+  });
+
+  test('each question has an answer answers', () => {
+    const lengthQuestions = wrapper.find('.title').length;
+    const lengthAnswers = wrapper.find('.answer').length;
+
+    expect(lengthQuestions).toBe(lengthAnswers);
+  });
+
+  test('clicking on a title activates entry', () => {
+    let firstQuestion = wrapper.find('.title').first();
+    const firstTitle = wrapper.find('.title-link').first();
+
+    expect(firstTitle.exists()).toBe(true);
+    expect(firstQuestion.hasClass('title--is-active')).toBe(false);
+
+    firstTitle.simulate('click', { preventDefault: () => {} });
+
+    wrapper.update();
+
+    firstQuestion = wrapper.find('.title').first();
+
+    expect(firstQuestion.hasClass('title--is-active')).toBe(true);
   });
 });
